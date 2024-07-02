28da75e1a44344c0278e86f625063f4acb7eaeaf
marinacr92 <marinacaro92@gmail.com>
2024-07-02 17:14:28 +0200

    Fin de sección 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
-@Component({
+@Component({   /// El decorador es una función que transforma mi clase para que sea un componente
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'bases';
+  public title: string = 'Hola Mundo';
+
 }
